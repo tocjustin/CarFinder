73dd48febc2e83ee84c82d1c32a92a648fbf9527
Alvin <alvinnguyen58@gmail.com>
2024-04-28 00:10:04 -0700

    Add search

--- backend/app.py
+++ backend/app.py
@@ -6,8 +6,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy.testing.suite.test_reflection import users
 from werkzeug.security import check_password_hash, generate_password_hash
-
-
 from backend.scrape import scrape_car_listings
 
 app = Flask(__name__)
@@ -31,7 +29,23 @@ class User(UserMixin, db.Model):
     lastName = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
-        return f'<User {self.username}>'
+        return f'<User {self.email}>'
+
+
+class Search(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    brand = db.Column(db.String(50))
+    model = db.Column(db.String(50))
+    year = db.Column(db.Integer)
+    price_range = db.Column(db.String(50))
+    mileage = db.Column(db.Integer)
+    zip_code = db.Column(db.String(10))
+    maximum_distance = db.Column(db.Integer)
+    user = db.relationship('User', backref=db.backref('alerts', lazy=True))
+
+    def __repr__(self):
+        return f'<Search {self.brand} {self.model} {self.year}>'
 
 
 @login_manager.user_loader
@@ -42,10 +56,10 @@ def user_loader(user_id):
 @login_manager.request_loader
 def request_loader(request):
     email = request.form.get('email')
-    if email not in users:
-        return None
-    user = User(email)
-    return user
+    user = User.query.filter_by(email=email).first()
+    if user:
+        return user
+    return None
 
 
 @app.route('/login', methods=['POST'])
@@ -101,6 +115,39 @@ def search():
     # Call the scraping function with these parameters
     results = scrape_car_listings(brand, model, year, price, mileage, zip_code, maximum_distance)
     return jsonify(results)
+
+
+@app.route('/save_search', methods=['POST'])
+@login_required
+def save_search():
+    data = request.get_json()
+
+    try:
+        brand = data.get('brand', '')
+        model = data.get('model', '')
+        year = int(data.get('year', 0))
+        price = int(data.get('price', 0))
+        mileage = int(data.get('mileage', 0))
+        zip_code = data.get('zip', '')
+        maximum_distance = int(data.get('maximumDistance', 0))
+
+        new_savedsearch = Search(
+            user_id=flask_login.current_user.id,
+            brand=brand,
+            model=model,
+            year=year,
+            price_range=price,
+            mileage=mileage,
+            zip_code=zip_code,
+            maximum_distance=maximum_distance
+        )
+        db.session.add(new_savedsearch)
+        db.session.commit()
+        return jsonify({"message": "Search registered successfully"}), 201
+
+    except ValueError:
+        return jsonify({"error": "Invalid input data"}), 400
+
 
 if __name__ == '__main__':
     app.run(debug=True)
